f552c83e29d5a5307da96e7e9027efa35ab709b4
gyabisito <josecitohernandez5@gmail.com>
2026-06-11 21:54:03 -0300

    fix oauth github + apiresponse en auth controller

--- database/migrations/03-create_users_table.php
+++ database/migrations/03-create_users_table.php
@@ -16,8 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique()->index();
             $table->string('password')->nullable();
-            $table->string('oauth_provider')->nullable();
-            $table->string('oauth_provider_id')->nullable();
             $table->enum('role', ['client', 'professional'])->default('client');
             $table->string('avatar_url')->nullable();
             $table->timestamp('email_verified_at')->nullable();
