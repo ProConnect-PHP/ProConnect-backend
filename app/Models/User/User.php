4fd91a8a4b4fc80b444974572322393f6dfbdea2
gyabisito <josecitohernandez5@gmail.com>
2026-06-14 17:25:03 -0300

    fix hashed user

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -37,7 +37,6 @@ class User extends Authenticatable implements JWTSubject
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
             'role' => UserRole::class,
             'password_changed_at' => 'datetime',
         ];
